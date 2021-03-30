485df33389d8b19f32d8cb7683d1c03baef64bac
Brian Kubes <briankubes@gmail.com>
2021-03-30 11:04:28 -0700

    getting started

---
+++ notes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from uuid import uuid4
+
+# Create your models here.
+class Note(models.Model):  # table in a database
+    # each of the variables are columns in the table
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+# this class inherets everything from Note class above
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
